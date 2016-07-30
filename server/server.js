1bb01ab3e077188bb4379b46a2a249810d3a133c
StevenYCChou <yencheng.chou@gmail.com>
2016-07-30 16:49:16 -0400

    parse body as json

--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@
 // PUT /games/:id/move
 
 var express = require('express'),
-    cookieParser = require('cookie-parser');
-
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser');
 // in memory values
 var nGames = 0;
 var nUsers = 0;
@@ -13,6 +13,7 @@ var games = [];
 
 var app = express();
 app.use(cookieParser());
+app.use(bodyParser.json());
 
 app.get('/games', function(req, res) {
   res.send(games);
@@ -40,6 +41,38 @@ app.get('/games/:id', function(req, res) {
 
 // assign user by using cookie
 app.get('/games/:id/assign', function(req, res) {
+  var gameId = req.params.id;
+  if (gameId < 0 || gameId >= nGames) {
+    res.status(404).send('Game not exists!');
+  }
+
+  var userId = req.cookies.userId;
+  if (userId < 0) {
+    res.status(400).send('Invalide user id!');
+  }
+
+  if (userId === undefined) {
+    userId = nUsers;
+    res.append('Set-Cookie', 'userId=' + userId + '; Path=/; HttpOnly');
+    nUsers++;
+  }
+
+  if (!games[gameId].hasOwnProperty('x')) {
+    games[gameId].x = userId;
+  } else if (games[gameId].x === userId) {
+    res.status(200).send('already assigned to game ' + gameId + '!');
+  } else if (!games[gameId].hasOwnProperty('o')) {
+    games[gameId].o = userId;
+  } else if (games[gameId].o === userId) {
+    res.status(200).send('already assigned to game ' + gameId + '!');
+  } else {
+    res.status(409).send('this game is full!');
+  }
+  res.status(200).send('get assigned to game ' + gameId + '!');
+});
+
+// assign user by using cookie
+app.put('/games/:id/move', function(req, res) {
   var gameId = req.params.id;
   if (gameId < 0 || gameId >= nGames) {
     res.status(404).send('Game not exists!');
