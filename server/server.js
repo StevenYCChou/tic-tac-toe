fe432269ff8f7056821d6f90ec9d4245c1b5c7c5
StevenYCChou <yencheng.chou@gmail.com>
2016-07-30 16:47:26 -0400

    use cookie to assign user to the board

--- server/server.js
+++ server/server.js
@@ -1,7 +1,73 @@
-// POST /game : create a new game
-// GET /game/:id current board
+// POST /games : create a new game
+// GET /games/:id current board status
+// GET /games/:id/join - join to game id. if cannot, will 4xx error.
+// PUT /games/:id/move
 
-// POST /game/:id/player/1 
-// POST /game/:id/player/2
-// PUT /game/:id/player/1/move
+var express = require('express'),
+    cookieParser = require('cookie-parser');
 
+// in memory values
+var nGames = 0;
+var nUsers = 0;
+var games = [];
+
+var app = express();
+app.use(cookieParser());
+
+app.get('/games', function(req, res) {
+  res.send(games);
+});
+
+app.post('/games', function(req, res) {
+  games.push({
+    id: nGames,
+    status: [0, 0, 0,
+             0, 0, 0,
+             0, 0, 0],
+    current_player: 'x'
+  });
+  nGames += 1;
+  res.status(200).end();
+});
+
+app.get('/games/:id', function(req, res) {
+  if (req.params.id >= 0 && req.params.id < nGames) {
+    res.send(games[req.params.id]);
+  } else {
+    res.status(404).send('Game not exists!');
+  }
+});
+
+// assign user by using cookie
+app.get('/games/:id/assign', function(req, res) {
+  var gameId = req.params.id;
+  if (gameId < 0 || gameId >= nGames) {
+    res.status(404).send('Game not exists!');
+  }
+
+  var userId = req.cookies.userId;
+  if (userId < 0) {
+    res.status(400).send('Invalide user id!');
+  }
+
+  if (userId === undefined) {
+    userId = nUsers;
+    res.append('Set-Cookie', 'userId=' + userId + '; Path=/; HttpOnly');
+    nUsers++;
+  }
+
+  if (!games[gameId].hasOwnProperty('x')) {
+    games[gameId].x = userId;
+  } else if (games[gameId].x === userId) {
+    res.status(200).send('already assigned to game ' + gameId + '!');
+  } else if (!games[gameId].hasOwnProperty('o')) {
+    games[gameId].o = userId;
+  } else if (games[gameId].o === userId) {
+    res.status(200).send('already assigned to game ' + gameId + '!');
+  } else {
+    res.status(409).send('this game is full!');
+  }
+  res.status(200).send('get assigned to game ' + gameId + '!');
+});
+
+app.listen(3000);
